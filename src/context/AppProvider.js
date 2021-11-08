fc03a61ca430401127170234b06bc1f1b672d624
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-11-08 22:15:17 +0000

    Requisito 15

--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -15,7 +15,7 @@ function Provider({ children }) {
   const [searchType, setSearchType] = useState('ingrediente');
   const [inputValueSearch, setInputValueSearch] = useState('');
 
-  async function searchRequest(type, inputValue) {
+  async function searchFoodRequest(type, inputValue) {
     let response = [];
     if (type === 'ingrediente') {
       response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${inputValue}`);
@@ -40,12 +40,38 @@ function Provider({ children }) {
     }
   }
 
+  async function searchDrinksRequest(type, inputValue) {
+    let response = [];
+    if (type === 'ingrediente') {
+      response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${inputValue}`);
+      const result = await response.json();
+      console.log(result);
+      return result;
+    }
+    if (type === 'nome') {
+      response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${inputValue}`);
+      const result = await response.json();
+      console.log(result);
+      return result;
+    }
+    if (type === 'primeira letra') {
+      if (inputValue.length !== 1) {
+        return global.alert('Sua busca deve conter somente 1 (um) caracter');
+      }
+      response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${inputValue}`);
+      const result = await response.json();
+      console.log(result);
+      return result;
+    }
+  }
+
   const context = {
     searchType,
     setSearchType,
     inputValueSearch,
     setInputValueSearch,
-    searchRequest,
+    searchFoodRequest,
+    searchDrinksRequest,
   };
 
   return (
