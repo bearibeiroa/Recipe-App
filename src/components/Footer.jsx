54c7a14cbddbd7703409f4de47e912cd703b05a2
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-11-11 17:06:48 +0000

    Voltando com os requisitos 22 e 24

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,20 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import exploreIcon from '../images/exploreIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
 import '../App.css';
-import AppContext from '../context/AppContext';
 
 function Footer() {
-  const { setBtnFilterCategory, setBtnFilterCategoryDrinks } = useContext(AppContext);
   return (
     <footer data-testid="footer" className="footer">
-      <Link to="/bebidas" onClick={ () => setBtnFilterCategoryDrinks(true) }>
+      <Link to="/bebidas">
         <img src={ drinkIcon } data-testid="drinks-bottom-btn" alt="Icone de drink" />
       </Link>
       <Link to="/explorar">
         <img src={ exploreIcon } data-testid="explore-bottom-btn" alt="Explorar" />
       </Link>
-      <Link to="/comidas" onClick={ () => setBtnFilterCategory(true) }>
+      <Link to="/comidas">
         <img src={ mealIcon } data-testid="food-bottom-btn" alt="Icone de comidas" />
       </Link>
     </footer>
