b104156d7221f294d06c315d34666d0f45e4a5b5
Bruno Trindade <brunotrndd@gmail.com>
2021-11-12 14:40:04 -0300

    Tela de explorar ingredientes

--- src/pages/IngredientesBebidas.js
+++ src/pages/IngredientesBebidas.js
@@ -1,14 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import IngredientCard from '../components/IngredientCard';
+import AppContext from '../context/AppContext';
 
 function IngredientesBebidas() {
   const [title] = useState('Explorar Ingredientes');
   const [haveSearch] = useState(false);
+  const [isFetch, setIsFetch] = useState(false);
+  const [ingredients, setIngredients] = useState([]);
+
+  const { setResultsDrinkApi } = useContext(AppContext);
+
+  async function requestIngredients() {
+    const TWELVE = 12;
+    const request = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
+    const result = await request.json();
+    setIngredients(result.drinks.slice(0, TWELVE));
+    setIsFetch(true);
+  }
+
+  useEffect(() => {
+    requestIngredients();
+  }, []);
+
+  async function searchByIngredient(name) {
+    const request = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`);
+    const result = await request.json();
+    console.log(result);
+    return result;
+  }
+
+  const history = useHistory();
+
+  async function handleClick(name) {
+    console.log(name);
+    const filteredDrinks = await searchByIngredient(name);
+    console.log(filteredDrinks);
+    setResultsDrinkApi(filteredDrinks);
+    history.push('/bebidas');
+  }
+
   return (
     <>
       <Header title={ title } haveSearch={ haveSearch } />
-      <h1>Página não encontrada :(</h1>
+      { isFetch ? ingredients.map(
+        (ingredient, index) => (
+          <button
+            type="button"
+            key={ index }
+            onClick={ () => handleClick(ingredient.strIngredient1) }
+          >
+            <IngredientCard
+              index={ index }
+              info={ ingredient }
+            />
+          </button>
+        ),
+      ) : null }
       <Footer />
     </>
   );
--- src/pages/IngredientesComidas.js
+++ src/pages/IngredientesComidas.js
@@ -1,14 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import IngredientCard from '../components/IngredientCard';
 
 function IngredientesComidas() {
   const [title] = useState('Explorar Ingredientes');
   const [haveSearch] = useState(false);
+  const [isFetch, setIsFetch] = useState(false);
+  const [ingredients, setIngredients] = useState([]);
+
+  async function requestIngredients() {
+    const TWELVE = 12;
+    const request = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?i=list');
+    const result = await request.json();
+    setIngredients(result.meals.slice(0, TWELVE));
+    setIsFetch(true);
+    console.log(result.meals.slice(0, TWELVE));
+  }
+
+  useEffect(() => {
+    requestIngredients();
+  }, []);
+
   return (
     <>
       <Header title={ title } haveSearch={ haveSearch } />
-      <h1>Página não encontrada :(</h1>
+      { isFetch ? ingredients.map(
+        (ingredient, index) => (
+          <IngredientCard
+            key={ index }
+            index={ index }
+            info={ ingredient }
+          />
+        ),
+      ) : null }
       <Footer />
     </>
   );
