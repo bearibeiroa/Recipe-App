eb338d8223e5b0ddb4b30d137f783dadfa958e81
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-11-12 13:12:20 +0000

    update commit

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -9,7 +9,12 @@ function RecipeCard(props) {
     const titleTestId = `${index}-card-name`;
     return (
       <section data-testid={ `${index}-recipe-card` }>
-        <img data-testid={ imgTestId } src={ strMealThumb } alt="Imagem da receita" />
+        <img
+          data-testid={ imgTestId }
+          src={ strMealThumb }
+          alt="Imagem da receita"
+          width="200"
+        />
         <p data-testid={ titleTestId }>{ strMeal }</p>
       </section>
     );
@@ -20,7 +25,12 @@ function RecipeCard(props) {
     const titleTestId = `${index}-card-name`;
     return (
       <section data-testid={ `${index}-recipe-card` }>
-        <img data-testid={ imgTestId } src={ strDrinkThumb } alt="Imagem da receita" />
+        <img
+          data-testid={ imgTestId }
+          src={ strDrinkThumb }
+          alt="Imagem da receita"
+          width="200"
+        />
         <p data-testid={ titleTestId }>{ strDrink }</p>
       </section>
     );
