648115d5b9cb8b06e8aa00e35a7b1fb0fde57561
Bruno Trindade <brunotrndd@gmail.com>
2021-11-13 15:23:46 -0300

    Ajuste req 83

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -1,18 +1,19 @@
-import React, { useContext, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import AppContext from '../context/AppContext';
+// import AppContext from '../context/AppContext';
 
 function Perfil() {
   const [title] = useState('Perfil');
   const [haveSearch] = useState(false);
-  const { userEmail } = useContext(AppContext);
+  // const { userEmail } = useContext(AppContext);
+  const mail = localStorage.getItem('user');
 
   return (
     <>
       <Header title={ title } haveSearch={ haveSearch } />
-      <h3 data-testid="profile-email">{ userEmail }</h3>
+      <h3 data-testid="profile-email">{ JSON.parse(mail).email }</h3>
       <Link to="/receitas-feitas">
         <button type="button" data-testid="profile-done-btn">Receitas Feitas</button>
       </Link>
