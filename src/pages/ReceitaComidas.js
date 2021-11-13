95ab790770e0f94abeea638e098078930c41e7fd
Bruno Trindade <brunotrndd@gmail.com>
2021-11-13 17:43:51 -0300

    Componentização da tela de detalhes da receita

--- src/pages/ReceitaComidas.js
+++ src/pages/ReceitaComidas.js
@@ -1,5 +1,7 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import RecipeDetails from '../components/RecipeDetails';
+import VideoCard from '../components/VideoCard';
 import AppContext from '../context/AppContext';
 
 function ReceitaComidas() {
@@ -11,23 +13,6 @@ function ReceitaComidas() {
     const response = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
     const result = await response.json();
     setApiResult(result.meals[0]);
-  }
-
-  function filterIngredients() {
-    const ingredients = Object.keys(apiResult);
-    const ingredientKeys = ingredients.filter((key) => key.includes('strIngredient'));
-    const measure = Object.keys(apiResult);
-    const measureKeys = measure.filter((key) => key.includes('strMeasure'));
-
-    return ingredientKeys.map((ingredient, index) => (
-      apiResult[ingredient] === ('' || null || '') ? null : (
-        <li
-          key={ index }
-          data-testid={ `${index}-ingredient-name-and-measure` }
-        >
-          {`${apiResult[ingredient]} - ${apiResult[measureKeys[index]]}`}
-        </li>)
-    ));
   }
 
   function recomendationCard() {
@@ -52,42 +37,10 @@ function ReceitaComidas() {
     fecthWithId();
   }, []);
 
-  useEffect(() => {
-    filterIngredients();
-  }, []);
-
   return (
     <main>
-      <img
-        data-testid="recipe-photo"
-        src={ `${apiResult.strMealThumb}` }
-        alt="Imagem da receita"
-        width="300"
-      />
-      <h1 data-testid="recipe-title">{ `${apiResult.strMeal}`}</h1>
-      <button data-testid="share-btn" type="button">Share</button>
-      <button data-testid="favorite-btn" type="button">Fav</button>
-      <h5 data-testid="recipe-category">{ `${apiResult.strCategory}`}</h5>
-      <h5>Ingredients</h5>
-      <ul>{ filterIngredients() }</ul>
-      <h5>Instructions</h5>
-      <p data-testid="instructions">
-        {' '}
-        {`${apiResult.strInstructions}`}
-      </p>
-      <h5>Video</h5>
-      <iframe
-        data-testid="video"
-        width="420"
-        height="315"
-        src={ `${apiResult.strYoutube}` }
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write;
-        encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="Embedded youtube"
-      />
-
+      <RecipeDetails data={ apiResult } />
+      <VideoCard data={ apiResult } />
       <h5>Recomendadas</h5>
       <div
         data-testid="recomendation-card"
@@ -102,7 +55,6 @@ function ReceitaComidas() {
       >
         Iniciar Receita
       </button>
-
     </main>
   );
 }
